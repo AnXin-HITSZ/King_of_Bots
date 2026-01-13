098919ce0229cae2af3ea8a1bee953683d3ceeec
AnXin-HITSZ <anxin.hitsz@outlook.com>
2026-01-13 21:01:54 +0800

    Added navigation bar function

---
+++ web/src/views/user/bot/UserBotIndexView.vue
@@ -0,0 +1,18 @@
+<template>
+    <ContentField>
+        我的 Bot
+    </ContentField>
+</template>
+
+<script>
+import ContentField from '../../../components/ContentField.vue';
+
+export default {
+    components: {
+        ContentField
+    }
+}
+</script>
+
+<style scoped>
+</style>
