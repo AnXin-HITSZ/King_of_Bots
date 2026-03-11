ff47571e71e30e9385bbb304a1785d90b0fcde21
AnXin-HITSZ <anxin.hitsz@outlook.com>
2026-03-11 21:38:40 +0800

    The project framework has been set up

---
+++ backend/src/main/java/com/kob/backend/config/CorsConfig.java
@@ -0,0 +1,56 @@
+package com.kob.backend.config;
+
+/**
+ * ClassName: CorsConfig
+ * Package: com.kob.backend.config
+ * Description:
+ *
+ * @Author AnXin
+ * @Create 2026/3/11 15:28
+ * @Version 1.0
+ */
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+
+/**
+ * CORS（跨域资源共享）过滤器，用于解决浏览器跨域访问限制问题
+ */
+@Configuration
+public class CorsConfig implements Filter {
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest request = (HttpServletRequest) req;
+
+        String origin = request.getHeader("Origin");
+        if(origin!=null) {
+            response.setHeader("Access-Control-Allow-Origin", origin);
+        }
+
+        String headers = request.getHeader("Access-Control-Request-Headers");
+        if(headers!=null) {
+            response.setHeader("Access-Control-Allow-Headers", headers);
+            response.setHeader("Access-Control-Expose-Headers", headers);
+        }
+
+        response.setHeader("Access-Control-Allow-Methods", "*");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
--- backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -3,6 +3,7 @@ package com.kob.backend.controller.pk;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -20,8 +21,11 @@ import java.util.Map;
 public class BotInfoController {
 
     @RequestMapping("getbotinfo/")
-    public List<Map<String, String>> getBotInfo(){
-        return null;
+    public Map<String, String> getBotInfo(){
+        Map<String, String> bot1 = new HashMap<>();
+        bot1.put("name", "apple");
+        bot1.put("rating", "1500");
+        return bot1;
     }
 
 }
